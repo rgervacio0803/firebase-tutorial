7c1882481dd7515d2f668fafcb1cc6a1426334ec
rgervacio0803 <ogturtel@yahoo.com>
2025-08-01 10:26:58 -0700

    first commit today

--- src/App.js
+++ src/App.js
@@ -1,57 +1,76 @@
-import React from 'react'
-import './App.css';
-import { auth } from './firebase/init';
-import { createUserWithEmailAndPassword,
+import React from "react";
+import "./App.css";
+import { auth, db } from "./firebase/init";
+import { collection, addDoc } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut, onAuthStateChanged,
- } from "firebase/auth";
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 
 function App() {
-  const [user, setUser] = React.useState({});
-  const [loading, setLoading] = React.useState(true)
+  const [user, setUser] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
 
-  React.useEffect (() => {
+  function createPost() {
+    const post = {
+      title: "Land a $100k job",
+      description: "Finish Frontend Simplified",
+    };
+    addDoc(collection(db, "post"), post);
+  }
+
+  React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      setLoading(false)
+      setLoading(false);
       console.log(user);
       if (user) {
-        setUser(user)
+        setUser(user);
       }
-    })
+    });
   }, []);
   function register() {
-    console.log('register');
-    createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
-    .then((user) => {
-      console.log(user)
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function login() {
-    signInWithEmailAndPassword(auth, 'email@email.com', 'test123')
-    .then(({user}) => {
-      console.log(user);
-      setUser(user);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    })
+    setLoading(true);
+    signInWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then(({ user }) => {
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
 
   function logout() {
-    signOut(auth)
-    setUser({})
+    signOut(auth);
+    setUser(null);
   }
 
   return (
     <div className="App">
-    <button onClick={register}>Register</button>
-    <button onClick={login}>Login</button>
-    <button onClick={logout}>Logout</button>
-    {loading ? 'loading...' : user.email}
+      {user ? (
+        <div className="user__profile" onClick={logout}>
+          {user.email[0].toUpperCase()}
+        </div>
+      ) : (
+        <>
+          <button onClick={register}>Register</button>
+          <button onClick={login}>{loading ? "Loading..." : "Login"}</button>
+          <button onClick={createPost}>Create Post</button>
+        </>
+      )}
     </div>
   );
 }
